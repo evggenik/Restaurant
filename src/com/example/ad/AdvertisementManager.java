7da9e363118ce29a41d54940348e7bf74f8cc019
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-21 22:43:41 +1000

    edited AdvertisementStorage and AdvertisementManager

--- src/com/example/ad/AdvertisementManager.java
+++ src/com/example/ad/AdvertisementManager.java
@@ -1,4 +1,7 @@
 package com.example.ad;
 
 public class AdvertisementManager {
+
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+
 }
--- src/com/example/ad/AdvertisementStorage.java
+++ src/com/example/ad/AdvertisementStorage.java
@@ -1,7 +1,20 @@
 package com.example.ad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdvertisementStorage {
+
+    private final List<Advertisement> videos = new ArrayList<>();
+
     private AdvertisementStorage() {
+        Object someContent = new Object();
+        add(new Advertisement(someContent, "First Video", 5000,
+                100, 3 * 60));
+        add(new Advertisement(someContent, "Second Video", 500,
+                10, 15 * 60));
+        add(new Advertisement(someContent, "Third Video", 400,
+                22, 10 * 60));
     }
 
     private static class InstanceHolder {
@@ -10,5 +23,13 @@ public class AdvertisementStorage {
 
     public static AdvertisementStorage getInstance() {
         return InstanceHolder.ourInstance;
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
     }
 }

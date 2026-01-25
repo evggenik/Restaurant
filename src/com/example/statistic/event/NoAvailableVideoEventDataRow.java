4d79b51e9cbb6e25469e48e2605eea048e74eb9b
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-25 12:44:47 +1000

    added event classes CookedOrderEventDataRow, NoAvailableVideoEventDataRow, VideoSelectedEventDataRow

---
+++ src/com/example/statistic/event/NoAvailableVideoEventDataRow.java
@@ -0,0 +1,13 @@
+package com.example.statistic.event;
+
+import java.util.Date;
+
+public class NoAvailableVideoEventDataRow implements EventDataRow {
+    private int totalDuration;
+    private Date currentDate;
+
+    public NoAvailableVideoEventDataRow(int totalDuration) {
+        this.totalDuration = totalDuration;
+        this.currentDate = new Date();
+    }
+}
---
+++ src/com/example/statistic/event/VideoSelectedEventDataRow.java
@@ -0,0 +1,21 @@
+package com.example.statistic.event;
+
+import com.example.ad.Advertisement;
+
+import java.util.Date;
+import java.util.List;
+
+public class VideoSelectedEventDataRow implements EventDataRow {
+    private List<Advertisement> optimalVideoSet;
+    private long amount;
+    private int totalDuration;
+    private Date currentDate;
+
+    public VideoSelectedEventDataRow(List<Advertisement> optimalVideoSet,
+                                     long amount, int totalDuration) {
+        this.optimalVideoSet = optimalVideoSet;
+        this.amount = amount;
+        this.totalDuration = totalDuration;
+        this.currentDate = new Date();
+    }
+}

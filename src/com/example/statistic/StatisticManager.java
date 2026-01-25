5eeeece4d8a85a5a2b2f65fc6c7f1c70a9f1d2a8
Evgeny Buchelnikov <gru554@yandex.ru>
2026-01-25 15:05:48 +1000

    added inner class StatisticStorage into StatisticManager

--- src/com/example/statistic/StatisticManager.java
+++ src/com/example/statistic/StatisticManager.java
@@ -1,8 +1,16 @@
 package com.example.statistic;
 
 import com.example.statistic.event.EventDataRow;
+import com.example.statistic.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticManager {
+
+    private final StatisticStorage statisticStorage = new StatisticStorage();
 
     private StatisticManager() {}
 
@@ -12,6 +20,16 @@ public class StatisticManager {
 
     public static StatisticManager getInstance() {
         return InstanceHolder.INSTANCE;
+    }
+
+    private class StatisticStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();;
+
+        public StatisticStorage() {
+            for (EventType eventType: storage.keySet()) {
+                this.storage.put(eventType, new ArrayList<>());
+            }
+        }
     }
 
     public void register(EventDataRow data) {
